8ada26bf8f85c23d3081dfaa64362785f6d32c1f
Germanchrystan <germanchrystangmail.com>
2026-02-16 03:06:25 -0300

    working rendering

--- Game1.cs
+++ Game1.cs
@@ -9,91 +9,92 @@ namespace POVMidiPlayer;
 
 public class Game1 : Game
 {
-    private GraphicsDeviceManager _graphics;
-    private SpriteBatch _spriteBatch;
-    private bool play = true;
-    private Player player;
-    private DynamicSoundEffectInstance dynSound;
-    private float[] synthBuffer;
-    private byte[] byteBuffer;
-    private int channelCount = 2; // Stereo
-    private int note = 60;
-    public Game1()
+  private GraphicsDeviceManager _graphics;
+  private SpriteBatch _spriteBatch;
+  private bool play = true;
+  private Player player;
+  private DynamicSoundEffectInstance dynSound;
+  private short[] synthBuffer;
+  private byte[] byteBuffer;
+  private int channelCount = 2; // Stereo
+  private int note = 60;
+  public Game1()
+  {
+    _graphics = new GraphicsDeviceManager(this);
+    Content.RootDirectory = "Content";
+    IsMouseVisible = true;
+  }
+
+  protected override void Initialize()
+  {
+    try
     {
-        _graphics = new GraphicsDeviceManager(this);
-        Content.RootDirectory = "Content";
-        IsMouseVisible = true;
+      base.Initialize();
+      dynSound = new DynamicSoundEffectInstance(44100, AudioChannels.Stereo);
+      dynSound.Play();
+
+    }
+    catch (Exception ex)
+    {
+      Console.WriteLine("Error during initialization: " + ex.Message);
+    }
+    synthBuffer = new short[2048 * channelCount];
+    byteBuffer = new byte[synthBuffer.Length * sizeof(short)];
+  }
+
+  protected override void LoadContent()
+  {
+    string sf2Path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "fluid.sf2");
+    _spriteBatch = new SpriteBatch(GraphicsDevice);
+    player = new Player(sf2Path);
+    player.ProgramSelect(0, 0, 0);
+    player.SetOutput(0, 44100, -6f);
+    player.SetVolume(1f);
+  }
+  private bool notePlaying = false;
+  private double noteOnTime = 0;
+  private double duration = 0.5;
+  protected override void Update(GameTime gameTime)
+  {
+    if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+      Exit();
+
+    if (!notePlaying && play)
+    {
+      player.NoteOn(0, note, 0.5f);
+      noteOnTime = gameTime.TotalGameTime.TotalSeconds;
+      notePlaying = true;
+      play = false;
     }
 
-    protected override void Initialize()
+    if (notePlaying)
     {
-        try
-        {
-            base.Initialize();
-            dynSound = new DynamicSoundEffectInstance(44100, AudioChannels.Stereo);
-            dynSound.Play();
-
-        } catch (Exception ex)
-        {
-            Console.WriteLine("Error during initialization: " + ex.Message);
-        }
-        synthBuffer = new float[2048 * channelCount];
-        byteBuffer = new byte[synthBuffer.Length * sizeof(float)];
+      double now = gameTime.TotalGameTime.TotalSeconds;
+      if (now - noteOnTime >= duration)
+      {
+        player.NoteOff(0, note);
+        notePlaying = false;
+      }
     }
 
-    protected override void LoadContent()
+    while (dynSound.PendingBufferCount < 2)
     {
-        string sf2Path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "fluid.sf2");
-        _spriteBatch = new SpriteBatch(GraphicsDevice);
-        player = new Player(sf2Path);
-        player.ProgramSelect(0, 0, 0);
-        player.SetOutput(0, 44100, -6f);
-        player.SetVolume(1f);
+      player.Render(synthBuffer);
+      // Console.WriteLine("Rendering audio buffer...");
+      // Console.WriteLine($"synthBuffer length: {synthBuffer.Length}");
+      // Console.WriteLine($"byteBuffer length: {byteBuffer.Length}");
+      Buffer.BlockCopy(synthBuffer, 0, byteBuffer, 0, synthBuffer.Length * sizeof(short));
+      dynSound.SubmitBuffer(byteBuffer);
     }
-    private bool notePlaying = false;
-    private double noteOnTime = 0;
-    private double duration = 0.5;
-    protected override void Update(GameTime gameTime)
-    {
-        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-            Exit();
+    base.Update(gameTime);
+  }
 
-       if (!notePlaying && play)
-        {
-            player.NoteOn(0, note, 0.5f);
-            noteOnTime = gameTime.TotalGameTime.TotalSeconds;
-            notePlaying = true;
-            play = false;
-        }
+  protected override void Draw(GameTime gameTime)
+  {
+    GraphicsDevice.Clear(Color.CornflowerBlue);
 
-        if (notePlaying)
-        {
-            double now = gameTime.TotalGameTime.TotalSeconds;
-            if(now - noteOnTime >= duration)
-            {
-                player.NoteOff(0, note);
-                notePlaying = false;
-            }
-        }
+    // TODO: Add your drawing code here
 
-        while(dynSound.PendingBufferCount < 2)
-        {
-            player.Render(synthBuffer);
-            // Console.WriteLine("Rendering audio buffer...");
-            // Console.WriteLine($"synthBuffer length: {synthBuffer.Length}");
-            // Console.WriteLine($"byteBuffer length: {byteBuffer.Length}");
-            Buffer.BlockCopy(synthBuffer, 0, byteBuffer, 0, synthBuffer.Length * sizeof(float));
-            dynSound.SubmitBuffer(byteBuffer);
-        }
-        base.Update(gameTime);
-    }
-
-    protected override void Draw(GameTime gameTime)
-    {
-        GraphicsDevice.Clear(Color.CornflowerBlue);
-
-        // TODO: Add your drawing code here
-
-        base.Draw(gameTime);
-    }
+    base.Draw(gameTime);
+  }
 }
--- tsf.cs
+++ tsf.cs
@@ -12,27 +12,24 @@ public static class Tsf
   public static extern void tsf_channel_set_bank(IntPtr tsf, int channel, int bank);
   [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
   public static extern void tsf_channel_set_presetnumber(IntPtr tsf, int channel, int preset, int mididrums);
-
   [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
   public static extern IntPtr tsf_load_filename(string filename);
-
   [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
   public static extern void tsf_close(IntPtr tsf);
-
   [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
   public static extern void tsf_note_on(IntPtr tsf, int channel, int key, float vel);
   [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
   public static extern void tsf_note_off(IntPtr tsf, int channel, int key);
   [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
   public static extern void tsf_render_float(IntPtr synth, float[] buffer, int numSamples, int flag);
-
+  [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+  public static extern void tsf_render_short(IntPtr synth, short[] buffer, int numSamples, int flag);
   [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
   public static extern string tsf_bank_get_presetname(IntPtr tsf, int bank, int preset_number);
   [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
   public static extern int tsf_get_presetcount(IntPtr tsf, int bank);
   [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
   public static extern void tsf_set_volume(IntPtr tsf, float global_gain);
-
   [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
   public static extern void tsf_set_output(IntPtr tsf, int outputmode, int samplerate, float global_gain_db);
 
@@ -61,9 +58,9 @@ public class Player : IDisposable
   {
     Tsf.tsf_note_off(this.synth, channel, key);
   }
-  public void Render(float[] synthBuffer)
+  public void Render(short[] synthBuffer)
   {
-    Tsf.tsf_render_float(this.synth, synthBuffer, synthBuffer.Length/ channelCount, 0);
+    Tsf.tsf_render_short(this.synth, synthBuffer, synthBuffer.Length / channelCount, 0);
   }
   public void ProgramSelect(int channel, int bank, int preset, bool isDrum = false)
   {
